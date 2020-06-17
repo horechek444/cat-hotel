a65823e561102cec4661fb6dd073a667d90d0c82
Tatiana Eskina <horechek444@gmail.com>
2020-06-17 14:18:33 +0300

    Адаптировать каталог под планшет

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -6,6 +6,9 @@ const thanksWindow = document.querySelector(".thanks");
 const thanksClose = thanksWindow.querySelector(".thanks__close");
 const thanksButton = thanksWindow.querySelector(".thanks__button");
 const modalButton = document.querySelector(".modal__button");
+const filterWindow = document.querySelector(".filter");
+const filterShow = document.querySelector(".rooms-list__filter-show");
+const filterClose = filterWindow.querySelector(".filter__close");
 
 /* Модальное окно каталога */
 let modalToggle = function(event) {
@@ -30,3 +33,11 @@ modalButton.addEventListener("click", thanksToggle);
 thanksClose.addEventListener("click", thanksToggle);
 thanksButton.addEventListener("click", thanksToggle);
 modalClose.addEventListener("click", thanksToggle);
+
+/* Окно каталога с фильтрами */
+let filtreToggle = function(event) {
+    filterWindow.classList.toggle("filter_show");
+}
+
+filterShow.addEventListener("click", filtreToggle);
+filterClose.addEventListener("click", filtreToggle);
