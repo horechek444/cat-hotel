fe2ae16f565552104424489b0a9c492d0b1cb27c
Tatiana Eskina <horechek444@gmail.com>
2020-07-17 15:02:43 +0300

    feat: доделать открытие меню на мобильном

--- scripts/header.js
+++ scripts/header.js
@@ -2,19 +2,18 @@ const header = document.querySelector('.header');
 const headerContainer = header.querySelector('.header__container');
 const headerButton = header.querySelector('.button__menu');
 
-const headerShow = function() {
-    header.classList.add('header_show');
-    headerContainer.classList.add('header__container_show');
-    headerButton.classList.remove('header__button');
-    headerButton.classList.add('header__button_show');
+const headerToggle = function() {
+    if (header.classList.contains('header_show')) {
+        header.classList.remove('header_show');
+        headerContainer.classList.remove('header__container_show');
+        headerButton.classList.add('header__button');
+        headerButton.classList.remove('header__button_show');
+    } else {
+        header.classList.add('header_show');
+        headerContainer.classList.add('header__container_show');
+        headerButton.classList.remove('header__button');
+        headerButton.classList.add('header__button_show');
+    }
 }
 
-const headerClose = function() {
-    header.classList.remove('header_show');
-    headerContainer.classList.remove('header__container_show');
-    headerButton.classList.add('header__button');
-    headerButton.classList.remove('header__button_show');
-    
-}
-
-headerButton.addEventListener('click', headerShow);
+headerButton.addEventListener('click', headerToggle);
