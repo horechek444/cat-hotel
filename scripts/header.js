39006866da5d38197256983a3187d56505bd6662
Tatiana Eskina <horechek444@gmail.com>
2020-07-17 17:34:15 +0300

    feat: сделать секцию main-banner

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,7 @@
 const header = document.querySelector('.header');
 const headerContainer = header.querySelector('.header__container');
 const headerButton = header.querySelector('.button__menu');
+const content = document.querySelector('.content');
 
 const headerToggle = function() {
     if (header.classList.contains('header_show')) {
@@ -8,11 +9,13 @@ const headerToggle = function() {
         headerContainer.classList.remove('header__container_show');
         headerButton.classList.add('header__button');
         headerButton.classList.remove('header__button_show');
+        content.classList.remove('content_type_menu');
     } else {
         header.classList.add('header_show');
         headerContainer.classList.add('header__container_show');
         headerButton.classList.remove('header__button');
         headerButton.classList.add('header__button_show');
+        content.classList.add('content_type_menu');
     }
 }
 
