7b77525dc5fc3a47d0177ad81bfdb585fa88498f
Tatiana Eskina <horechek444@gmail.com>
2021-02-07 13:04:54 +0300

    feat: мобильная версия каталога

--- scripts/header.js
+++ scripts/header.js
@@ -1,7 +1,6 @@
 const header = document.querySelector('.header');
 const headerContainer = header.querySelector('.header__container');
 const headerButton = header.querySelector('.button__menu');
-const content = document.querySelector('.content');
 
 const headerToggle = function() {
     if (header.classList.contains('header_show')) {
@@ -9,13 +8,11 @@ const headerToggle = function() {
         headerContainer.classList.remove('header__container_show');
         headerButton.classList.add('header__button');
         headerButton.classList.remove('header__button_show');
-        content.classList.remove('content_type_menu');
     } else {
         header.classList.add('header_show');
         headerContainer.classList.add('header__container_show');
         headerButton.classList.remove('header__button');
         headerButton.classList.add('header__button_show');
-        content.classList.add('content_type_menu');
     }
 }
 
